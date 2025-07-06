c6df119027f5baafe79b89f0c2aac78d87b6533e
homa-ae <147973198+homa-ae@users.noreply.github.com>
2025-07-06 21:46:37 +0200

    Update clean_missing_values.py

--- clean_missing_values.py
+++ clean_missing_values.py
@@ -1,3 +1,18 @@
-print(merged_df.isnull().sum())  # Shows missing values per column (before cleaning)
-merged_df = merged_df.dropna()  # Removes all rows that have at least one missing value 
-print(merged_df.isnull().sum())  # Shows missing values per column (after cleaning)
+# Shows missing values per column (before cleaning)
+print(merged_df.isnull().sum())
+
+# Removes all rows that have at least one missing value 
+merged_df = merged_df.dropna()  
+
+# Shows missing values per column (after cleaning)
+print(merged_df.isnull().sum())  
+
+
+# Print the number of rows before filtering
+print("Before:", len(merged_df))
+
+# Keep only rows where the 'Label' column is 0 or 1
+merged_df = merged_df[merged_df['Label'].isin([0, 1])]
+
+# Print the number of rows after filtering
+print("After:", len(merged_df))
