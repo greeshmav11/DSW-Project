1a88ae038b7d2cb0444a0e5bf95239359a123bfe
homa-ae <147973198+homa-ae@users.noreply.github.com>
2025-07-08 17:48:55 +0200

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -16,3 +16,5 @@ merged_df = merged_df[merged_df['Label'].isin([0, 1])]
 
 # Print the number of rows after filtering
 print("After:", len(merged_df))
+
+merged_df.to_csv('data/cleaned_merged_news_market.csv', index=False)
